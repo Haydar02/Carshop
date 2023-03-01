4d08168b26ee9a31e797590b5812f0679a6a8670
Haydar02 <hayd0281@edu.easj.dk>
2023-03-01 20:51:47 +0100

    Add project files.

---
+++ Carshop/Models/Car.cs
@@ -0,0 +1,55 @@
+﻿namespace Carshop.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string? Model { get; set; }
+
+        public double Price { get; set; }
+
+        public string? LicensPlat { get; set; }
+
+
+        public void ModelValidat()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException("Model dose not be null");
+            }
+
+            if (Model.Length != 3)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public void PriceValdiate()
+        {
+            if (Price <= 0)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+        }
+        public void LicensePlatvalidate()
+
+        {
+            if (LicensPlat == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (LicensPlat.Length <= 1)
+            {
+                throw new ArgumentException();
+            }
+            if (LicensPlat.Length != 8)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+
+    }
+}
---
+++ CarshopTests/CarTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carshop.Models;
+
+namespace Carshop.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car { Id = 1, Model = "Bmw", Price = 150000, LicensPlat = "PD 25565" };
+        Car modelnull = new Car { Id = 1, Model = "BM", Price = 150000, LicensPlat = "PD 25565" };
+        Car Emptymodel = new Car { Id = 1, Model = null, Price = 150000, LicensPlat = "PD 25565" };
+        Car PriceNull = new Car { Id = 1, Model = "BM", Price = 0, LicensPlat = "PD 25565" };
+        Car EmptyLicensPlat = new Car { Id = 1, Model = "Bmw", Price = 150000, LicensPlat = null };
+        Car licensplatsmallerThanOne = new Car { Id = 1, Model = "BM", Price = 0, LicensPlat = "p" };
+        Car LicensplateMoreThanSeven = new Car { Id = 1, Model = "Bmw", Price = 150000, LicensPlat = "pp02565699" };
+
+        [TestMethod()]
+        public void ModelValidatTest()
+        {
+            car.ModelValidat();
+
+           Assert.ThrowsException<ArgumentNullException>(() => Emptymodel.ModelValidat());
+
+           Assert.ThrowsException<ArgumentException>(() => modelnull.ModelValidat());
+        }
+        [TestMethod()]
+        public void PriceValidateTest()
+        {
+            car.PriceValdiate();
+            {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() =>PriceNull.PriceValdiate());
+            }
+        }
+        [TestMethod]
+        public void LicensPlatValidateTest() 
+        {
+          car.LicensePlatvalidate();
+
+            Assert.ThrowsException<ArgumentNullException>(()=> EmptyLicensPlat.LicensePlatvalidate());
+            Assert.ThrowsException<ArgumentException>(()=> licensplatsmallerThanOne.LicensePlatvalidate());   
+            Assert.ThrowsException<ArgumentException>(()=> LicensplateMoreThanSeven.LicensePlatvalidate());
+        }
+
+    }
+}
